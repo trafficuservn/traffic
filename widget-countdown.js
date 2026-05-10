f8cd1a87e446a41a8aee40a78724dfd03df399c5
Traffic User <info.trafficuser@gmail.com>
2026-05-10 17:29:21 +0700

    Update widget-countdown.js

--- widget-countdown.js
+++ widget-countdown.js
@@ -102,7 +102,7 @@
             cursor: not-allowed;
         }
         .custom-button-${CONTAINER_ID} img {
-            height: 40px;
+            height: 20px;
             margin-right: 5px;
             display: inline-block;
             width: auto;
