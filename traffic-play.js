1a8bec8a9accfd5b55e07c0fd798955f1e1011c5
Traffic User <info.trafficuser@gmail.com>
2026-06-22 17:41:36 +0700

    Update traffic-play.js

--- traffic-play.js
+++ traffic-play.js
@@ -9,7 +9,7 @@
     // Các cấu hình tích hợp thêm cho tính năng cuộn trang
     let isPausedByScroll = false;
     let scrollTimeout;
-    const SCROLL_STOP_DELAY = 10000; // 10 giây không cuộn sẽ báo dừng
+    const SCROLL_STOP_DELAY = 35000; // 10 giây không cuộn sẽ báo dừng
     const SCROLL_ALERT_MESSAGE = 'Vui lòng thực hiện thao tác cuộn để tiếp tục đếm ngược thời gian.';
     
     const REF_DOMAIN_LIST = ["google.com","google.ad","google.ae","google.com.af","google.com.ag","google.com.ai","google.al","google.am","google.co.ao","google.com.ar","google.as","google.at","google.com.au","google.az","google.ba","google.com.bd","google.be","google.bf","google.bg","google.com.bh","google.bi","google.bj","google.com.bn","google.com.bo","google.com.br","google.bs","google.bt","google.co.bw","google.by","google.com.bz","google.ca","google.cd","google.cf","google.cg","google.ch","google.ci","google.co.ck","google.cl","google.cm","google.cn","google.com.co","google.co.cr","google.com.cu","google.cv","google.com.cy","google.cz","google.de","google.dj","google.dk","google.dm","google.com.do","google.dz","google.com.ec","google.ee","google.com.eg","google.es","google.com.et","google.fi","google.com.fj","google.fm","google.fr","google.ga","google.ge","google.gg","google.com.gh","google.com.gi","google.gl","google.gm","google.gr","google.com.gt","google.gy","google.com.hk","google.hn","google.hr","google.ht","google.hu","google.co.id","google.ie","google.co.il","google.im","google.co.in","google.iq","google.is","google.it","google.je","google.com.jm","google.jo","google.co.jp","google.co.ke","google.com.kh","google.ki","google.kg","google.co.kr","google.com.kw","google.kz","google.la","google.com.lb","google.li","google.lk","google.co.ls","google.lt","google.lu","google.lv","google.com.ly","google.co.ma","google.md","google.me","google.mg","google.mk","google.ml","google.com.mm","google.mn","google.ms","google.com.mt","google.mu","google.mv","google.mw","google.com.mx","google.com.my","google.co.mz","google.com.na","google.com.ng","google.com.ni","google.ne","google.nl","google.no","google.com.np","google.nr","google.nu","google.co.nz","google.com.om","google.com.pa","google.com.pe","google.com.pg","google.com.ph","google.com.pk","google.pl","google.pn","google.com.pr","google.ps","google.pt","google.com.py","google.com.qa","google.ro","google.ru","google.rw","google.com.sa","google.com.sb","google.sc","google.se","google.com.sg","google.sh","google.si","google.sk","google.com.sl","google.sn","google.so","google.sm","google.sr","google.st","google.com.sv","google.td","google.tg","google.co.th","google.com.tj","google.tl","google.tm","google.tn","google.to","google.com.tr","google.tt","google.com.tw","google.co.tz","google.com.ua","google.co.ug","google.co.uk","google.com.uy","google.co.uz","google.com.vc","google.co.ve","google.vg","google.co.vi","google.com.vn","google.vu","google.ws","google.rs","google.co.za","google.co.zm","google.co.zw","google.cat"];
@@ -109,7 +109,7 @@
             max-width: none !important;
             max-height: none !important;
             padding: 10px 18px !important;
-            font-size: 14px !important;
+            font-size: 20px !important;
             background: rgba(238, 47, 46, 0.95) !important;
             border: 2px solid yellow !important;
             color: yellow !important;
@@ -121,7 +121,7 @@
             max-width: none !important;
             max-height: none !important;
             padding: 8px 16px !important;
-            font-size: 14px !important;
+            font-size: 20px !important;
         }
         .custom-button-${CONTAINER_ID}.disabled-state {
             cursor: not-allowed !important;
@@ -158,7 +158,7 @@
             transform: translate(-50%, -50%) !important;
             padding: 15px 25px !important;
             background: rgba(238, 47, 46, 0.98) !important;
-            color: yellow !important;
+            color: #ffffff !important;
             font-weight: 700 !important;
             font-size: 16px !important;
             border-radius: 10px !important;
