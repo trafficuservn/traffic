a39b0454b7b31481343c2872e98e7270263df0fc
Traffic User <info.trafficuser@gmail.com>
2026-06-22 15:48:28 +0700

    Update traffic-play.js

--- traffic-play.js
+++ traffic-play.js
@@ -83,7 +83,7 @@
             position: relative !important;
             box-shadow: 0 3px 8px rgba(0,0,0,0.25) !important;
             font-weight: 700 !important;
-            font-size: 16px !important;
+            font-size: 24px !important;
             line-height: 1 !important;
         }
         
