626142b7dc9ed80b4d1232efe1f793ffeb3dc18f
Traffic User <info.trafficuser@gmail.com>
2026-08-08 13:20:04 +0700

    Update traffic-play.js

--- traffic-play.js
+++ traffic-play.js
@@ -97,8 +97,8 @@
         }
         .custom-button-${CONTAINER_ID} svg {
             box-sizing: border-box !important;
-            width: 40px !important; /* Điều chỉnh lại tỉ lệ icon play cho vừa nút tròn */
-            height: 40px !important;
+            width: 54px !important;
+            height: 54px !important;
             fill: #ffffff !important;
             display: block !important;
             margin: 0 !important;
