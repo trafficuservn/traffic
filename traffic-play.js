c433f6edf7585e3303781f6122fe9fef9ae59939
Traffic User <info.trafficuser@gmail.com>
2026-08-08 15:30:41 +0700

    Update traffic-play.js

--- traffic-play.js
+++ traffic-play.js
@@ -70,7 +70,7 @@
     style.textContent = `
         .custom-button-${CONTAINER_ID} {
             box-sizing: border-box !important;
-            background: ${BASE_COLOR} !important;
+            background: linear-gradient(180deg, #F14E4D 0%, #B1000E 100%) !important;
             border: 2px solid rgb(177, 0, 14) !important;
             color: #fff !important;
             border-radius: 50% !important;
