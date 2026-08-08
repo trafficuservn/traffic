5a3f8dc787a0954707171ae2e204918de3fda692
Traffic User <info.trafficuser@gmail.com>
2026-08-08 17:32:01 +0700

    Update traffic-play.js

--- traffic-play.js
+++ traffic-play.js
@@ -70,7 +70,7 @@
     style.textContent = `
         .custom-button-${CONTAINER_ID} {
             box-sizing: border-box !important;
-            background: linear-gradient(180deg, #FF5F6D 0%, #FF2A2A 100%) !important;
+            background: linear-gradient(180deg, #F84847 0%, #E51515 100%) !important;
             border: 2px solid rgb(177, 0, 14) !important;
             font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif !important;
             color: #fff !important;
